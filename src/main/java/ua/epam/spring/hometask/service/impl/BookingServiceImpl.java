d4fe91574dcc8676d8eaeea3f172c16429c55881
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2017-08-21 15:15:48 +0200

    Add logic to BookingServiceImpl

--- src/main/java/ua/epam/spring/hometask/service/impl/BookingServiceImpl.java
+++ src/main/java/ua/epam/spring/hometask/service/impl/BookingServiceImpl.java
@@ -1,29 +1,69 @@
 package ua.epam.spring.hometask.service.impl;
 
-import ua.epam.spring.hometask.domain.Event;
-import ua.epam.spring.hometask.domain.Ticket;
-import ua.epam.spring.hometask.domain.User;
+import ua.epam.spring.hometask.domain.*;
 import ua.epam.spring.hometask.service.BookingService;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.NavigableMap;
 import java.util.Set;
 
 public class BookingServiceImpl implements BookingService {
+
+
+    private HashSet<Ticket> bookedTickets = new HashSet<>();
+
     @Override
     public double getTicketsPrice(@Nonnull Event event, @Nonnull LocalDateTime dateTime, @Nullable User user, @Nonnull Set<Long> seats) {
-        return 0;
+        Auditorium auditorium = getAuditorium(event, dateTime);
+
+        double basePrice = event.getBasePrice();
+        double totalPrice = 0;
+        double ticketPrice = 0;
+
+        for (Long seatNumber : seats) {
+            ticketPrice = getTicketPrice(auditorium, basePrice, seatNumber);
+
+            if (EventRating.HIGH.equals(event.getRating())) {
+                ticketPrice *= 1.2;
+            }
+
+            totalPrice += ticketPrice;
+        }
+
+        DiscountServiceImpl discountService = new DiscountServiceImpl();
+
+        return totalPrice - totalPrice * discountService.getDiscount(user, event, dateTime, seats.size()) / 100;
+    }
+
+    private double getTicketPrice(Auditorium auditorium, double basePrice, Long seatNumber) {
+        if (isVipSeat(auditorium, seatNumber)) {
+            return basePrice * 2;
+        }
+
+        return basePrice;
+    }
+
+    private boolean isVipSeat(Auditorium auditorium, Long seatNumber) {
+        Set<Long> vipSeats = auditorium.getVipSeats();
+        return vipSeats.contains(seatNumber);
+    }
+
+    private Auditorium getAuditorium(Event event, LocalDateTime dateTime) {
+        NavigableMap<LocalDateTime, Auditorium> auditoriums = event.getAuditoriums();
+        return auditoriums.get(dateTime);
     }
 
     @Override
     public void bookTickets(@Nonnull Set<Ticket> tickets) {
-
+        bookedTickets.addAll(tickets);
     }
 
     @Nonnull
     @Override
     public Set<Ticket> getPurchasedTicketsForEvent(@Nonnull Event event, @Nonnull LocalDateTime dateTime) {
-        return null;
+        return bookedTickets;
     }
 }
---
+++ src/test/java/ua/epam/spring/hometask/service/impl/BookingServiceImplTest.java
@@ -0,0 +1,104 @@
+package ua.epam.spring.hometask.service.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+import ua.epam.spring.hometask.domain.Auditorium;
+import ua.epam.spring.hometask.domain.Event;
+import ua.epam.spring.hometask.domain.EventRating;
+import ua.epam.spring.hometask.domain.Ticket;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created on 8/21/2017.
+ */
+public class BookingServiceImplTest {
+
+    private Event event;
+    private LocalDateTime dateTime;
+    private BookingServiceImpl bookingService;
+
+    @Before
+    public void setUp() throws Exception {
+        bookingService = new BookingServiceImpl();
+
+        event = new Event();
+        event.setBasePrice(100);
+
+        TreeMap<LocalDateTime, Auditorium> auditoriums = new TreeMap<>();
+        dateTime = LocalDateTime.now();
+        Auditorium auditorium = new Auditorium();
+        HashSet<Long> vipSeats = new HashSet<>();
+        vipSeats.add(5L);
+        auditorium.setVipSeats(vipSeats);
+        auditoriums.put(dateTime, auditorium);
+        event.setAuditoriums(auditoriums);
+    }
+
+    @Test
+    public void givenBasePriceReturnBasePrice() throws Exception {
+        HashSet<Long> seats = new HashSet<>();
+        seats.add(1L);
+
+        double actualPrice = bookingService.getTicketsPrice(event, LocalDateTime.now(), null, seats);
+
+        assertEquals(100, actualPrice, 0);
+    }
+
+    @Test
+    public void givenBasePriceReturnBasePriceForTwoTickets() throws Exception {
+        HashSet<Long> seats = new HashSet<>();
+        seats.add(1L);
+        seats.add(2L);
+
+        double actualPrice = bookingService.getTicketsPrice(event, LocalDateTime.now(), null, seats);
+
+        assertEquals(200, actualPrice, 0);
+    }
+
+    @Test
+    public void givenBasePriceReturnVipPrice() throws Exception {
+        HashSet<Long> seats = new HashSet<>();
+        seats.add(5L);
+
+        double actualPrice = bookingService.getTicketsPrice(event, dateTime, null, seats);
+
+        assertEquals(200, actualPrice, 0);
+    }
+
+    @Test
+    public void givenBasePriceAndHighRatedMovieReturnHighRatedPrice() throws Exception {
+        event.setRating(EventRating.HIGH);
+        HashSet<Long> seats = new HashSet<>();
+        seats.add(1L);
+
+        double actualPrice = bookingService.getTicketsPrice(event, dateTime, null, seats);
+
+        assertEquals(120, actualPrice, 0);
+    }
+
+    @Test
+    public void bookTickets() throws Exception {
+        HashSet<Ticket> tickets = new HashSet<>();
+        tickets.add(new Ticket(null, event, dateTime, 1L));
+        bookingService.bookTickets(tickets);
+    }
+
+    @Test
+    public void getPurchasedTicketsForEvent() throws Exception {
+        HashSet<Ticket> tickets = new HashSet<>();
+        Ticket ticket = new Ticket(null, event, dateTime, 1L);
+        tickets.add(ticket);
+        bookingService.bookTickets(tickets);
+
+        Set<Ticket> purchasedTickets = bookingService.getPurchasedTicketsForEvent(event, dateTime);
+
+        assertTrue(purchasedTickets.contains(ticket));
+    }
+
+}
