36eedbc662088e58292cb11ddf35e4716cf67960
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2017-08-29 22:01:59 +0200

    Add db connection

--- src/main/java/ua/epam/spring/hometask/dao/UserDAO.java
+++ src/main/java/ua/epam/spring/hometask/dao/UserDAO.java
@@ -1,14 +1,16 @@
 package ua.epam.spring.hometask.dao;
 
+import org.springframework.jdbc.core.RowMapper;
+import ua.epam.spring.hometask.domain.DomainObject;
 import ua.epam.spring.hometask.domain.User;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
-public class UserDAO extends DomainObjectDAO<User>{
+public class UserDAO extends DomainObjectDAO<User> {
     public @Nullable
     User getUserByEmail(@Nonnull String email) {
         for (User user : domainObjects) {
@@ -18,5 +20,54 @@ public class UserDAO extends DomainObjectDAO<User>{
         }
 
         return null;
+    }
+
+    @Override
+    public boolean save(User object) {
+        String sql = "insert into " + tableName + " " +
+                "(id, msg, firstName, lastName, email) values (?, ?, ?, ?, ?)";
+
+        jdbcTemplate.update(sql, object.getId(), "some message",
+                object.getFirstName(), object.getLastName(), object.getEmail());
+        return domainObjects.add(object);
+    }
+
+    @Override
+    public boolean remove(User object) {
+        return super.remove(object);
+    }
+
+    @Override
+    public User getById(@Nonnull Long id) {
+        String sql = "select id, msg, firstName, lastName, email from " + tableName + " where id=?";
+
+        return jdbcTemplate.queryForObject(sql, (resultSet, rowNumber) -> {
+            User user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setFirstName(resultSet.getString("firstName"));
+            user.setLastName(resultSet.getString("lastName"));
+            user.setEmail(resultSet.getString("email"));
+            return user;
+        }, id);
+    }
+
+    @Nonnull
+    @Override
+    public Collection<User> getAll() {
+        return super.getAll();
+    }
+
+    @Override
+    public void init() throws SQLException {
+        if (!isTableExist(tableName)) {
+            String sql = "create table " + tableName + " " +
+                    "(id integer, " +
+                    "msg varchar(255), " +
+                    "firstName varchar(50), " +
+                    "lastName varchar(50), " +
+                    "email varchar(50))";
+
+            jdbcTemplate.execute(sql);
+        }
     }
 }
