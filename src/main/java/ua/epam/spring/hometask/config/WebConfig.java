477ad24886e46ef857728ba09429045a01c070ce
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2017-09-05 20:27:35 +0200

    Switch to java config

---
+++ src/main/java/ua/epam/spring/hometask/config/WebConfig.java
@@ -0,0 +1,52 @@
+package ua.epam.spring.hometask.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.support.ControllerClassNameHandlerMapping;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+import ua.epam.spring.hometask.App;
+
+@Configuration
+@EnableWebMvc
+@Import(AppConfig.class)
+@ComponentScan("ua.epam.spring.hometask.mvc")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+/*    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+
+        return resolver;
+    }*/
+
+    @Bean
+    public ControllerClassNameHandlerMapping controllerClassNameHandlerMapping() {
+        return new ControllerClassNameHandlerMapping();
+    }
+
+    @Bean
+    public FreeMarkerViewResolver freeMarkerViewResolver() {
+        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+        resolver.setCache(true);
+        resolver.setPrefix("");
+        resolver.setSuffix(".ftl");
+        return resolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("/WEB-INF/freemarker/");
+        return configurer;
+    }
+}
