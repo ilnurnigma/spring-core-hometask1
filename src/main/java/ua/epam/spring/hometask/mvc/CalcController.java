1694f5404f4b81ad70df44ff6a27385c7dbe01d1
Ilnur Nigmatzyanov <ilnur.nigma@gmail.com>
2017-09-05 16:59:08 +0200

    Add additional request mapping in CalcController

--- src/main/java/ua/epam/spring/hometask/mvc/CalcController.java
+++ src/main/java/ua/epam/spring/hometask/mvc/CalcController.java
@@ -1,8 +1,10 @@
 package ua.epam.spring.hometask.mvc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import ua.epam.spring.hometask.service.BookingService;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServletRequest;
  */
 @Controller
 public class CalcController {
+    @Autowired
+    BookingService bookingService;
 
     // [GET] http://host.com/example/calculate?first=123&second=456
     @RequestMapping("/calculate")
@@ -20,6 +24,17 @@ public class CalcController {
         int firstInt = Integer.parseInt(first);
         int secondInt = Integer.parseInt(second);
         ModelAndView mav = new ModelAndView("displaySum");
+        mav.addObject("sum", Integer.toString(firstInt + secondInt));
+        return mav;
+    }
+
+    @RequestMapping("/calculate2")
+    public ModelAndView calculate2(HttpServletRequest request) {
+        String first = request.getParameter("first");
+        String second = request.getParameter("second");
+        int firstInt = Integer.parseInt(first);
+        int secondInt = Integer.parseInt(second);
+        ModelAndView mav = new ModelAndView("displaySum2");
         mav.addObject("sum", Integer.toString(firstInt + secondInt));
         return mav;
     }
---
+++ src/main/webapp/WEB-INF/freemarker/displaySum2.ftl
@@ -0,0 +1,8 @@
+<html>
+<head></head>
+
+<body>
+<h1>This is the body of the sample view</h1>
+<P>${sum}</P>
+</body>
+</html>
